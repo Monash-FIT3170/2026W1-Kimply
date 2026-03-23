0dce7e5c5d76f189d986771bb5b3264e7a0ab450
Dhruv Israni <dhruv.isr14@gmail.com>
2026-03-23 12:30:01 +1100

    Database connection and showing data completed. *Task Schema required to complete DB operations

--- tutorial/ToDoApp/imports/ui/App.jsx
+++ tutorial/ToDoApp/imports/ui/App.jsx
@@ -1,7 +1,31 @@
+import React from "react";
+import { useTracker , useSubscribe } from "meteor/react-meteor-data";
+import { TasksCollection } from "../api/collections/TasksCollection";
 
-export const App = () => (
-  <div className="page">
-    <main className="main">
-    </main>
-  </div>
-);
+
+
+export const App = () => {
+
+  const isLoading = useSubscribe("tasks");
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+
+  if (isLoading()){
+    return <div>Loading...</div>
+  }
+
+
+  return (
+    <div>
+      <h1> Welcome to Meteor! </h1>
+
+      <ul>
+        {tasks.map((task)=>(
+          <h1>{task._id}</h1>
+        ))}
+      </ul>
+    </div>
+  )
+
+
+};
