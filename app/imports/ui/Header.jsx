407a2731e620243483ccf0ef0c4789433af410d4
Layela <lmoy0001@student.monash.edu>
2026-05-07 14:02:52 +1000

    Removed meteor ui, initialised react project

--- app/imports/ui/Header.jsx
+++ app/imports/ui/Header.jsx
@@ -1,13 +1,8 @@
-import MeteorLogo from "./meteor-logo.svg";
-
 export const Header = () => {
   return (
     <div className="header">
       <nav className="nav container">
-        <div className="logo-container">
-          <MeteorLogo className="logo" />
-        </div>
-        <h1 className="page-title">Welcome to Meteor!</h1>
+        <h1 className="page-title">Kimply</h1>
       </nav>
     </div>
   );
