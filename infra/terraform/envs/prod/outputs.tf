d3476c5f8c3de7029ffc9817772749393ce89f97
R4chC0dE <mriannguyen352@gmail.com>
2026-09-23 18:10:22 +1000

    [issue 1] fix(infra): re-export nat_gateway_id from the prod root module
    envs/dev reads it through terraform_remote_state, which only sees root
    module outputs. The module exposed it but envs/prod did not, so dev failed
    with "This object does not have an attribute named nat_gateway_id".

--- infra/terraform/envs/prod/outputs.tf
+++ infra/terraform/envs/prod/outputs.tf
@@ -29,3 +29,8 @@ output "github_deploy_role_arn" {
 output "github_ecr_push_role_arn" {
   value = module.kimply.github_ecr_push_role_arn
 }
+
+# Read by envs/dev through remote state: development borrows this NAT (D40).
+output "nat_gateway_id" {
+  value = module.kimply.nat_gateway_id
+}
